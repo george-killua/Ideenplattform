aa0d028f4cb5fda82915a1e248744938fc4e71aa
george kassih <george.kassih@edu.fh-kaernten.ac.at>
2021-07-19 19:50:00 +0200

    api initialized

--- app/src/main/java/com/killua/ideenplattform/data/requests/CreateCommentReq.kt
+++ app/src/main/java/com/killua/ideenplattform/data/requests/CreateCommentReq.kt
@@ -1,5 +1,6 @@
 package com.killua.ideenplattform.data.requests
 
-data class CreateCommentReq (    val message:String
+data class CreateCommentReq(
+    val message: String
 //example: Really cool idea!
 )
---
+++ app/src/main/java/com/killua/ideenplattform/data/requests/ResponseHandler.kt
@@ -0,0 +1,3 @@
+package com.killua.ideenplattform.data.requests
+
+data class ResponseHandler(val code: Int, val description: String, val data: Any)
